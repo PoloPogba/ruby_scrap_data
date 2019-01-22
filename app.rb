4afc11a74478917a8d0014790c89dcd69018bbd3
alts27 <45126585+alts27@users.noreply.github.com>
2019-01-22 20:26:15 +0100

    voila

--- app.rb
+++ app.rb
@@ -2,29 +2,24 @@ require 'bundler'
 Bundler.require
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'app/scrapper.rb'
-require 'views/index.rb'
-require 'views/done.rb'
 require 'open-uri'
 require 'csv'
 require 'pp'
 
-
-class Launch
-
-    puts "Choisis le truc que tu veux faire!  Pour voir JSON, tappes "JSON"! Pour le Google Sheet, tappes "GOOGLE"! Pour le fichier CSV, tappes "CSV"!"
+class Start
+    puts "Choisis le truc que tu veux faire!  Pour voir JSON, tappes JSON! Pour le Google Sheet, tappes GOOGLE! Pour le fichier CSV, tappes CSV!"
     print ">"
     choix = gets.chomp
-    when "JSON"
+    if choix =="JSON"
     Townhall.save_as_JSON
     puts "woohoo! le fichier vient d'être créé!"
-    when "GOOGLE"
+    elsif choix =="GOOGLE"
     Townhall.save_as_spreadsheet
     puts "woohoo! le fichier vient d'être créé! tu peux le trouver dans https://docs.google.com/spreadsheets/d/1dShvJQ8MyP8ZkrNgno0Aj_qurjmhKd5450rsdwxTSNI/edit#gid=0"
-    when "CSV"
+    elsif choix =="CSV"
     Townhall.save_as_csv
     puts "woohoo! le fichier vient d'être créé!"
     end
-
 end
 
-Launch.new
+Start
